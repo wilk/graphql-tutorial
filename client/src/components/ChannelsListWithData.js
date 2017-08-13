b9774cb954263718fa15e524cf81b28094a9dbd5
Ramya Nagarajan <ramyanaga.99@gmail.com>
2017-08-12 23:28:53 -0700

    WIP: printing props, but doesn't have correct data

--- client/src/components/ChannelsListWithData.js
+++ client/src/components/ChannelsListWithData.js
@@ -9,35 +9,41 @@ import {
 } from 'react-apollo';
 
 import AddChannel from './AddChannel';
+import LoadMore from './LoadMore';
 
-const ChannelsList = ({ data: {loading, error, channels }}) => {
-  if (loading) {
-    return <p>Loading ...</p>;
-  }
-  if (error) {
-    return <p>{error.message}</p>;
-  }
+//const ChannelsList = ({ data: {loading, error, channels }}) => {
+const ChannelsList = (props) => {
+  console.log('props: ', props)
+  // if (loading) {
+  //   return <p>Loading ...</p>;
+  // }
+  // if (error) {
+  //   return <p>{error.message}</p>;
+  // }
 
-  return (
-    <div className="channelsList">
-      <AddChannel />
-      { channels.map( ch =>
-        (<div key={ch.id} className={'channel ' + (ch.id < 0 ? 'optimistic' : '')}>
-          <Link to={ch.id < 0 ? `/` : `channel/${ch.id}`}>
-            {ch.name}
-          </Link>
-        </div>)
-      )}
-      <button type="button">Load More</button>
-      {/* change to :
-        <button OnClick={onClickFunction}>
-          Load More
-        </button>
-       */}
-    </div>
+  // console.log(this.props.channels)
+  // return (
+  //   <div className="channelsList">
+  //     <AddChannel />
+  //     { channels.map( ch =>
+  //       (<div key={ch.id} className={'channel ' + (ch.id < 0 ? 'optimistic' : '')}>
+  //         <Link to={ch.id < 0 ? `/` : `channel/${ch.id}`}>
+  //           {ch.name}
+  //         </Link>
+  //       </div>)
+  //     )}
+  //     {/*<button type="button">Load More</button>*/}
+  //       {/* <button OnClick={testFetchMore}>
+  //         Load More
+  //       </button> */}
+  //   </div>
 
-  );
+  // );
 };
+
+ChannelsList.propType = {
+  test: React.PropTypes.func
+}
 
 export const channelsListQuery = gql`
   query ChannelsListQuery($offset: Int, $limit: Int) {
@@ -50,12 +56,20 @@ export const channelsListQuery = gql`
 
 
 export default graphql(channelsListQuery, {
-  options: { 
-    //pollInterval: 5000
+  options: {
+    pollInterval: 5000,
     variables: {
       offset: 0,
-      limit: 1
+      limit: 2
     }
   },
+  // apollo client will automatically adds loading, data, and the result of the query
+  props: (props) => {
+    return Object.assign(props, {
+      testFunc: () => {
+        console.log('hello')
+      }
+    })
+  } 
 })(ChannelsList);
 

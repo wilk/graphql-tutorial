56543f7872688d1eb2be5f161cbc7e2d3bc16a1a
Ramya Nagarajan <ramyanaga.99@gmail.com>
2017-08-14 10:39:08 -0700

    Unpack channels differently

--- client/src/components/ChannelsListWithData.js
+++ client/src/components/ChannelsListWithData.js
@@ -11,34 +11,69 @@ import {
 import AddChannel from './AddChannel';
 import LoadMore from './LoadMore';
 
+
+
+
+
+
+
+
+
+
+
+/*
+ * props: {
+ *  data: {
+ *    loading,
+ *    errors
+ *    channels
+ *  }
+ * }
+ */
+
+
+
+/*
+const ChannelsList = props => {
+  const { data } = props;
+  const data = props.data;
+  const { loading, errors, channels } = data;
+  const loading = data.loading;
+*/
+
+
 //const ChannelsList = ({ data: {loading, error, channels }}) => {
-const ChannelsList = (props) => {
-  console.log('props: ', props)
-  // if (loading) {
-  //   return <p>Loading ...</p>;
-  // }
-  // if (error) {
-  //   return <p>{error.message}</p>;
-  // }
+const ChannelsList = props => {
+  console.log('props: ', props);
+  const data = props.data
+  const { loading, error, channels } = data;
+  //console.log(loading)
+  //console.log('props: ', props)
+  //console.log('data: ', data)
+  if (loading) {
+    return <p>Loading ...</p>;
+  }
+  if (error) {
+    return <p>{error.message}</p>;
+  }
 
-  // console.log(this.props.channels)
-  // return (
-  //   <div className="channelsList">
-  //     <AddChannel />
-  //     { channels.map( ch =>
-  //       (<div key={ch.id} className={'channel ' + (ch.id < 0 ? 'optimistic' : '')}>
-  //         <Link to={ch.id < 0 ? `/` : `channel/${ch.id}`}>
-  //           {ch.name}
-  //         </Link>
-  //       </div>)
-  //     )}
-  //     {/*<button type="button">Load More</button>*/}
-  //       {/* <button OnClick={testFetchMore}>
-  //         Load More
-  //       </button> */}
-  //   </div>
+  return (
+    <div className="channelsList">
+      <AddChannel />
+      { channels.map( ch =>
+        (<div key={ch.id} className={'channel ' + (ch.id < 0 ? 'optimistic' : '')}>
+          <Link to={ch.id < 0 ? `/` : `channel/${ch.id}`}>
+            {ch.name}
+          </Link>
+        </div>)
+      )}
+      {/*<button type="button">Load More</button>*/}
+        {/* <button OnClick={testFetchMore}>
+          Load More
+        </button> */}
+    </div>
 
-  // );
+  );
 };
 
 ChannelsList.propType = {
@@ -65,6 +100,7 @@ export default graphql(channelsListQuery, {
   },
   // apollo client will automatically adds loading, data, and the result of the query
   props: (props) => {
+    console.log(props)
     return Object.assign(props, {
       testFunc: () => {
         console.log('hello')
